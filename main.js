499c89ad20c32b4f1594e2893ca0f8a0d53d6a29
HafizN <hafizn2013@outlook.com>
2022-11-06 21:40:16 -0800

    merge two sorted lists

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
 //import {Heap} from './Heap.js';
 
 function hello() {
-  //alert("hello world")
-  console.log("testing sampleFunc")
-  let value = sampleFunc(5, 5)
-  //value should be 25
-  console.assert(25 == value, value)
-  let data = generateData()
-  console.log("data size " + data.length)
-  console.timeLog("generate time", data)
-  console.log("before sort first 10 values")
-  printFirstTenValues(data)
-  bubble(data)
-  console.log("after sort")
-  console.log("after sort first 10 values")
-  printFirstTenValues(data)
-  //try heap
-  let data2 = generateData()
-  printFirstTenValues(data2)
-  console.log("after sort")
-  const heap = new Heap()
-  for (var i = 0; i < data2.length; i++) {
-    heap.insert(data2[i])
-  }
-  console.log("heap size " + heap.heapSize())
-  for(var j = 0; j < 500; j++) {
-    console.log("pop heap " + heap.pop())
-  }
-  console.log("heap after size " + heap.heapSize())
+  // //alert("hello world")
+  // console.log("testing sampleFunc")
+  // let value = sampleFunc(5, 5)
+  // //value should be 25
+  // console.assert(25 == value, value)
+  // let data = generateData()
+  // console.log("data size " + data.length)
+  // console.timeLog("generate time", data)
+  // console.log("before sort first 10 values")
+  // printFirstTenValues(data)
+  // bubble(data)
+  // console.log("after sort")
+  // console.log("after sort first 10 values")
+  // printFirstTenValues(data)
+  // //try heap
+  // let data2 = generateData()
+  // printFirstTenValues(data2)
+  // console.log("after sort")
+  // const heap = new Heap()
+  // for (var i = 0; i < data2.length; i++) {
+  //   heap.insert(data2[i])
+  // }
+  // console.log("heap size " + heap.heapSize())
+  // for(var j = 0; j < 500; j++) {
+  //   console.log("pop heap " + heap.pop())
+  // }
+  // console.log("heap after size " + heap.heapSize())
   
+  mergeTwoLists()
 }
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+function mergeTwoLists(l1, l2) {
+  var mergedHead = { val : -1, next : null },
+  crt = mergedHead;
+  while(l1 && l2) {
+    if (l1.val > l2.val) {
+        crt.next = l2;
+        l2 = l2.next;
+    } else {
+        crt.next = l1;
+        l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
+}
+
 
 function testLinkedList() {
   let data = generateData();
